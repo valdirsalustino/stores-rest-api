e3f7c9bdc57c158a400478912ade7306be678913
Valdir Salustino <valdir.salustino@ibm.com>
2021-10-26 08:29:25 -0300

    fix: adjust db dependence
    - fix the db dependence for first request
    
    Signed-off-by: Valdir Salustino <valdir.salustino@ibm.com>

--- app.py
+++ app.py
@@ -15,11 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'abcd1234'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-	db.create_all()
-
 jwt = JWT(app, authenticate, identity) # /auth
 
 api.add_resource(Item, '/item/<string:name>')
