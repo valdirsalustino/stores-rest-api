14b5774d4a46baf0acd939b4b400b27a42709fd4
Valdir Salustino <valdir.salustino@ibm.com>
2021-10-28 17:46:46 -0300

    fix: fix store model:
    - fixing the story model;
    
    Signed-off-by: Valdir Salustino <valdir.salustino@ibm.com>

--- models/store.py
+++ models/store.py
@@ -3,7 +3,7 @@ from db import db
 class StoreModel(db.Model):
 
 	__tablename__ = 'stores'
-	id = db.Column(db.Integer(), primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(80))
 	items = db.relationship('ItemModel', lazy='dynamic')
 
